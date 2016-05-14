12853ef8b92b7c123cad765f6c295808d5208bb2
Max Humberstone <maxhumberstonedesigns@gmail.com>
2016-05-14 18:50:30 +0100

    Revert to working nav

--- js/main.js
+++ js/main.js
@@ -1,41 +1,12 @@
 $(function(){
-
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('header').outerHeight();
-
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down
-            $('header').removeClass('nav-down').addClass('nav-up');
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('header').removeClass('nav-up').addClass('nav-down');
-            }
-        }
-
-        lastScrollTop = st;
-    }
+        $(".type").typed({
+            strings: ["a user experience", "a visual", "an industrial"],
+            typeSpeed: 100,
+            loop: true,
+            startDelay: 1000,
+            // backspacing speed
+            backSpeed: 100,
+            // time before backspacing
+            backDelay: 3000,
+        });
 });
