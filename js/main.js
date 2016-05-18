eb8a2aebb0436c493b64b9a8606a0e7e2a0e4e16
Max Humberstone <maxhumberstonedesigns@gmail.com>
2016-05-18 21:27:30 +0100

    Mobile optimized home page and actor website added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 $(function(){
 
-  $(".type").typed({
-      strings: ["a user experience", "a visual", "an industrial"],
-      typeSpeed: 100,
-      loop: true,
-      startDelay: 1000,
-      // backspacing speed
-      backSpeed: 100,
-      // time before backspacing
-      backDelay: 3000,
-  });
-
-
+  if ($(this).width() > 480) {
+      // call supersize method
+      $(".type").typed({
+          strings: ["a user experience", "a visual", "an industrial"],
+          typeSpeed: 100,
+          loop: true,
+          startDelay: 1000,
+          // backspacing speed
+          backSpeed: 100,
+          // time before backspacing
+          backDelay: 3000,
+      });
+   }
 
 });
